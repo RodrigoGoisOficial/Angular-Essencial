001c2e30a4ac5955b3cdaa76f07067779e6c4d20
Rodrigo Gois <rodrigojosedegois@gmail.com>
2023-01-14 23:03:37 -0300

    finalizing product creation component

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
-import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-create',
@@ -9,6 +10,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit{
+
+  product: Product = {
+    name: '',
+    price: null
+  }
 
   constructor(
     private productService: ProductService,
@@ -20,11 +26,14 @@ export class ProductCreateComponent implements OnInit{
   }
 
   createProduct(): void {
-    this.productService.showMessage("Produto criado com sucesso!")
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado com sucesso!');
+      this.router.navigate(['/products']);
+    });
   }
 
   cancel(): void {
-    this.router.navigate(['/products'])
+    this.router.navigate(['/products']);
   }
 
 }
